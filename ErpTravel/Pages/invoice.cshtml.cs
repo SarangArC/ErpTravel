a4085a4f276d6bfa6099dd6e7d62e442d8f04cdb
sarangsaraz@gmail.com <sarangsaraz@gmail.com>
2024-10-09 19:14:45 +0530

    Added the invoice page

---
+++ ErpTravel/Pages/invoice.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ErpTravel.Pages
+{
+    public class invoiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
